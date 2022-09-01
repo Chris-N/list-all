beab58c8d9a028972e5a563c4d0ec504bcfd69d0
Chris-N <sdchrisn@gmail.com>
2022-09-01 12:12:14 -0700

    Unit test for add button is rendered

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header', () => {
   render(<App />);
   const textElement = screen.getByText(/Stuff/i);
+  expect(textElement).toBeInTheDocument();
+});
+
+test('renders add button', () => {
+  render(<App />);
+  const textElement = screen.getByText(/add item/i);
   expect(textElement).toBeInTheDocument();
 });
