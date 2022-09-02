538b882957f538babb9dc3f07f25c0901f8a4e79
Chris-N <sdchrisn@gmail.com>
2022-09-01 17:13:03 -0700

    A quick catch all route screen to return user back home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Home } from './components/Home/Home';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { AddItem } from 'components/AddItem/AddItem';
 
 function App() {
@@ -9,6 +9,15 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/add' element={<AddItem />} />
+        <Route
+          path='*'
+          element={
+            <div>
+              <p>Please return home</p>
+              <Link to='/'>Home</Link>
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
