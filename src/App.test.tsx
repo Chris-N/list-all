5dc211d08dbaf0f2d494c98d744bf73d54f3dd29
Chris-N <sdchrisn@gmail.com>
2022-11-12 12:10:13 -0800

    Remove the remaining files from the resturcture

--- src/App.test.tsx
+++
@@ -1,5 +0,0 @@
-import '@testing-library/react';
-
-test.todo('full app rendering and check route to add screen');
-
-test.todo('full app rendering and check route to catch all screen');
--- src/components/ListItem/ListItem.test.tsx
+++
@@ -1,2 +0,0 @@
-// TODO: Add unit tests to new component
-export {};
