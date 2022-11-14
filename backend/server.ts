f61e1be25703443ca4a8e25aae850cf60d1c3b38
Chris-N <sdchrisn@gmail.com>
2022-11-13 17:18:01 -0800

    Test resource route to get sample initial data for the frontend

--- backend/server.ts
+++ backend/server.ts
@@ -10,6 +10,23 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World! From Typescript TESTING!');
 });
 
+interface Item {
+  name: string;
+  category: string;
+  isComplete: boolean;
+  createDatetime?: Date;
+}
+app.get('/test', (req: Request, res: Response) => {
+  const test: Item[] = [
+    { name: 'Apples', category: 'Produce', isComplete: false },
+    { name: 'Bananas', category: 'Produce', isComplete: true },
+    { name: 'Peaches', category: 'Produce', isComplete: false },
+    { name: 'Donuts', category: 'Bakery', isComplete: false },
+  ];
+
+  res.send(test);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
